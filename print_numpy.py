75782eb063fc460bf6ef27e0dc579240f174477f
HudsonNock <hudsonnock111@gmail.com>
2025-11-24 12:40:48 -0800

    new data

--- print_numpy.py
+++ print_numpy.py
@@ -3,6 +3,8 @@ import numpy as np
 
 
 if __name__ == "__main__":
-    arr = np.load("data/actions_oldp.npy")
-    print(arr[1090:1190])
+    arr = np.load("data/actions_overhead.npy")
+    #arr = arr[:386]
+    #np.save("data/actions_overhead.npy", arr)
+    print(arr[:,4].cumsum()[-1])
     #print(np.where(arr[:,4] > 0.02))
